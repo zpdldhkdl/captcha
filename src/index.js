3883f3e78b01c08c060fed18be8cc289afc220a2
DESKTOP-A43AVN4\kyong <lklklnm@daum.net>
2020-09-25 00:11:44 +0900

    add html

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const svgCaptcha = require('./lib/svg-captcha');
+const router = require('./router/index');
 
 dotenv.config();
 
@@ -15,9 +16,13 @@ app.get('/captcha', async (req, res) => {
 });
 
 // views set
-app.set('views', __dirname + 'views');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 const PORT = process.env.PORT || 3000;
+
+router(app);
 
 app.listen(PORT, () => {
     console.log(`express server has started on port ${PORT}`);
